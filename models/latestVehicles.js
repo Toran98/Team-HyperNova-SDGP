2d8c21eff13ce94cbce3d73d3e3135db88801477
Sandaaa97 <62935007+Sandaaa97@users.noreply.github.com>
2020-05-04 08:22:53 +0530

    Update latestVehicles.js

--- models/latestVehicles.js
+++ models/latestVehicles.js
@@ -7,6 +7,15 @@ var latestVehicles = mongoose.model('latestvehicles',{
     company:{type: String,required:true},
     price:{type: String},
     engineCapacity:{type: String},
+    fuelType:{type: String},
+    transmission:{type: String},
+    age:{type: String},
+    mileage:{type: String},
+    priceOneYear:{type:String},
+    priceTwoYear:{type:String},
+    priceFiveYearOne:{type:String},
+    priceFiveYearTwo:{type:String},
+    priceFiveYearThree:{type:String}
 
 });
 
