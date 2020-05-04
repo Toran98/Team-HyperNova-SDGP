a6975336ba4d578577c731c3d65a6158bae02c63
Sandaaa97 <62935007+Sandaaa97@users.noreply.github.com>
2020-05-04 08:24:04 +0530

    Update vehicles.js

--- models/vehicles.js
+++ models/vehicles.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
 
 const vehicles = mongoose.model('vehicles',{
-    id:{type: String, required:true}, 
-    model:{type: String, required:true},
-    year:{type: String, required:true},
-    company:{type: String, required:true},
-    price:{type: String, required:true},
-    engineCapacity:{type: String, required:true},
-
+    id:{type: String}, 
+    model:{type: String},
+    year:{type: String},
+    company:{type: String,required:true},
+    price:{type: String},
+    engineCapacity:{type: String},
+    fuelType:{type: String},
+    transmission:{type: String},
+    age:{type: String},
+    mileage:{type: String},
+    priceOneYear:{type:String},
+    priceTwoYear:{type:String},
+    priceFiveYearOne:{type:String},
+    priceFiveYearTwo:{type:String},
+    priceFiveYearThree:{type:String}
 });
 
 module.exports = {vehicles} ;
